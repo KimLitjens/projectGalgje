bebc712cf020691a2b76654cc0d95f67b213a1ea
kracok <kracok@gmail.com>
2020-02-28 17:17:42 +0100

    style shanges

--- script.test.js
+++ script.test.js
@@ -3,8 +3,3 @@ const functions = require("./script.js");
 test('the wordPicker is not empty', () => {
   expect(functions.wordPicker()).not.toBeFalsy();
 });
-
-test('the game is over', () => {
-    expect(functions.winTheGame()).toBeTruthy();
-});
-
